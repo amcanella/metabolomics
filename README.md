9b75d665e916b4fb305321fa6237239a27ab2dc0
amcanella <alomc97@gmail.com>
2025-02-25 19:29:18 +0100

    Update to instructions and low field folder

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# metabolomics
-Repo to store the metabolomic scripts for the TB fingerprint
+# Metabolomics
+1H NMR spectra simulation based on official databases. 
+
+- By inputting a xlsx or csv file with names, peak parameters and concentrations of metabolites, user can replicate any spectra in low and high field NMRs.
+- The script is currently prepared for urine samples as the centre of the peaks will shift.
--- Spectra_generator/low_field/call_simulator_LF.py
+++ Spectra_generator/low_field/call_simulator_LF.py
@@ -11,7 +11,7 @@ import time
 
 import random 
 
-p = process_data('C:/Repos/low_field_db.xlsx')
+p = process_data('C:/Repos/compiled_data_2025-01-07-06.xlsx') #'low_field_db.xlsx')
 d = p.create_dict()
 s = Simulator(dictionary = d, met_data=p.met_data, clust_data = p.clust_data, clust_dict=p.clust_dict())
 
@@ -20,13 +20,13 @@ print(f'this is a test {p.met_data[0][6]}')
 NFREQ = 23_122 #ZEROS REMOVED 65536
 SAMPLES = 1#0_000
 #NO UREA, 64 nor cholate 37 for 292 peaks
-#mets = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,38,39,40,41,42,43,44,45,46,47,48,49,50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,61,62,63,65,66,67] #np.arange(1,26) #[3,4,5]
-mets = [1,2,3,4,5,7,9,22]
+mets = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,38,39,40,41,42,43,44,45,46,47,48,49,50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,61,62,63,65,66,67] #np.arange(1,26) #[3,4,5]
+#mets = [1,2,3,4,5,7,9,22]
 # mets = [3,4,5]
 nmet = len(mets)
 noise = 0.001              #0.0001
 
-for j in range(1,11):
+for j in range(1,2):
     
     print(j)
     spectrum = [0]*SAMPLES
